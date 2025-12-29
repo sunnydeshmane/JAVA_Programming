d6f252b13618ce68ce551229ffb5d03c2154f2db
SunnyDeshmane. <sunnydeshmane2004@gmail.com>
2025-12-29 20:21:55 +0530

    Add files via upload

---
+++ Classwork/program72.java
@@ -0,0 +1,17 @@
+class program72
+{
+    public static void main(String A[])
+    {
+        int iNo = 12;
+        int i = 0;
+
+        for(i=1; i<=(iNo / 2);i++)
+        {
+            if((iNo) % i==0)
+            {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
